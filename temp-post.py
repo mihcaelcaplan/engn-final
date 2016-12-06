369e18716c893338010c1356bf485fdecbb1b747
michael <michael_caplan@email.com>
2016-12-06 16:00:08 -0500

    changed method

--- temp-post.py
+++ temp-post.py
@@ -1,4 +1,4 @@
 from w1thermsensor import W1ThermSensor
 
-for sensor in W1ThermSensor.get_available_sensors():
-    print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+sensor = W1ThermSensor()
+temperature_in_celsius = sensor.get_temperature()
