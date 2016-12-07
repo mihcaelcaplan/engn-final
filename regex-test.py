b5351e2296943b1aa204e1cb4331b134ea660bce
michael <michael_caplan@email.com>
2016-12-06 19:11:59 -0500

    added regex to serial parsing

---
+++ regex-test.py
@@ -0,0 +1,11 @@
+import re
+
+line1 = "1: 21.56"
+line2 = "1: 22.562: 45.32"
+
+# print re.findall('1: ([1-9.]{5})', line2)
+# print re.findall('2: ([1-9.]{5})', line2)
+
+list1 = ["hello", "hi"]
+
+print list1[1]
